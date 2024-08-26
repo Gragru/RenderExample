da61818ca332a8f049c08362c6fdd1b73890f4f6
Mikael Engström <micke@primat.se>
2024-08-26 15:53:54 +0200

    Initial commit

---
+++ RenderExample/Models/Block.cs
@@ -0,0 +1,16 @@
+﻿namespace RenderExample.Models
+{
+    public class Block
+    {
+
+        public Block(int id, string type, string content)
+        {
+            Id= id;
+            Type= type;
+            Content= content;
+        }
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Content { get; set; }
+    }
+}
